4b6888a4da9c7717390e4b0ffa4ee8bb61038937
Uxianity <djavo.016@gmail.com>
2025-05-15 18:20:43 +0200

    Create LuauX.lua

---
+++ content/LuauX.lua
@@ -0,0 +1,23 @@
+--[[
+
+©️ 2025 LuauX - Author: Uxianity 
+Luau-X Main
+
+This module follows the MIT license.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Credit is appreciated, not enforced.
+
+You're able to review the code freely as provided.
+If you're in the need to modify the code, you'll be responsible for all issues, bugs, that can, or cause destruction.
+If new to coding or new to applying modifications, keep the module as it is.
+
+]]--
+
